e4d15de0476dad82e7dee064a77507f133061a95
Philippe Ferdinand <pshampanier@gmail.com>
2013-10-17 19:42:25 -0400

    adding function addVariable

--- psns/js/delivery.js
+++ psns/js/delivery.js
@@ -7,11 +7,19 @@
  **************************************************************************/
 psns.delivery  = psns.delivery || (function() {
 
+  var mapDataType = { 
+    string:     { value: 6, name: 'stringValue' },
+    long:       { value: 3, name: 'longValue' },
+    double:     { value: 5, name: 'doubleValue' },
+    timestamp:  { value: 7, name: 'timeStampValue' },
+    memo:       { value: 12, name: 'memoValue' }
+  };
+
   return {
   
-    /** Add or replace a SMTP header.
+    /** Add or set a SMTP header.
       * 
-      * @delivery a delivery object.
+      * @delivery a delivery object (not XML).
       * @header   the header name
       * @value    the header value.
       * @return   the previous value of the header. */
@@ -35,6 +43,39 @@ psns.delivery  = psns.delivery || (function() {
     
       delivery.mailParameters.headers = headers;
       return previousValue;
+    },
+    
+    /** Add or set a variable.
+      * 
+      * @delivery a delivery object (not XML).
+      * @name     variable name.
+      * @type     variable data type (see mapDataType above).
+      * @value    variable value.
+      * @return   the previous value of the variable. */
+    addVariable: function(delivery, name, type, value) {
+    
+      // check the datatype
+      var typeInfo = mapDataType[type];
+      if ( !typeInfo ) {
+        psns.error("Invalid datatype '%1'", type);
+      }
+
+      // seach the variable
+      for (var i=0; i < delivery.variables._var.length; i++) {
+        var v = delivery.variables._var[i];
+        if ( v.name === name ) {
+          // existing variable
+          var previousValue = v[typeInfo.name];
+          v.dataType        = typeInfo.value;
+          v[typeInfo.name]  = value;
+          return previousValue;
+        }
+      }
+      
+      // variable not found, we are creating a new one
+      var v = delivery.variables._var.add(<var name={name} dataType={typeInfo.value}/>);
+      v[typeInfo.name]  = value;
+      return undefined;
     }
   
   }
